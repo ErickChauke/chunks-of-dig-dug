4d34f09a2a9ec4984f7fd1497b3aca2ce4da857d
Erick Chauke <erickchauke0217@gmail.com>
2025-09-15 18:14:07 +0200

    Add basic EnemyLogic implementation

---
+++ game-source-code/EnemyLogic.cpp
@@ -0,0 +1,33 @@
+#include "EnemyLogic.h"
+#include <cstdlib>
+#include <ctime>
+
+EnemyLogic::EnemyLogic() : previousMove(Direction::NONE), blockedCount(0) {
+    static bool seeded = false;
+    if (!seeded) {
+        std::srand(static_cast<unsigned>(std::time(nullptr)));
+        seeded = true;
+    }
+}
+
+Coordinate EnemyLogic::getDirectionOffset(Direction dir) const {
+    switch (dir) {
+        case Direction::UP:    return Coordinate(-1, 0);
+        case Direction::DOWN:  return Coordinate(1, 0);
+        case Direction::LEFT:  return Coordinate(0, -1);
+        case Direction::RIGHT: return Coordinate(0, 1);
+        case Direction::NONE:  return Coordinate(0, 0);
+    }
+    return Coordinate(0, 0);
+}
+
+Direction EnemyLogic::findDirectionToward(Coordinate from, Coordinate to) const {
+    int deltaRow = to.row - from.row;
+    int deltaCol = to.col - from.col;
+    
+    if (std::abs(deltaRow) > std::abs(deltaCol)) {
+        return (deltaRow > 0) ? Direction::DOWN : Direction::UP;
+    } else {
+        return (deltaCol > 0) ? Direction::RIGHT : Direction::LEFT;
+    }
+}

3489aa42de02bd6bcb773171098f769ec6efe376
Erick Chauke <erickchauke0217@gmail.com>
2025-09-28 13:31:51 +0200

    feat: add PowerUp collection implementation

--- game-source-code/PowerUpManager.cpp
+++ game-source-code/PowerUpManager.cpp
@@ -27,6 +27,49 @@ void PowerUpManager::updatePowerUpEffects() {
     harpoonCooldown = hasRapidFire ? 0.3f : 1.0f;
 }
 
+void PowerUpManager::collectPowerUp(const PowerUp& powerUp, Player& player, 
+                                   int& playerLives, int& score) {
+    lastPowerUpCollected = GetTime();
+    
+    switch (powerUp.getType()) {
+        case PowerUpType::EXTRA_LIFE:
+            playerLives++;
+            powerUpMessage = "Extra Life Gained!";
+            break;
+            
+        case PowerUpType::SCORE_MULTIPLIER:
+            score += 500;
+            powerUpMessage = "+500 Score Bonus!";
+            break;
+            
+        case PowerUpType::RAPID_FIRE:
+            activePowerUps.emplace_back(PowerUpType::RAPID_FIRE, 15.0f);
+            powerUpMessage = "Rapid Fire Activated!";
+            break;
+            
+        case PowerUpType::POWER_SHOT:
+            activePowerUps.emplace_back(PowerUpType::POWER_SHOT, 20.0f);
+            powerUpMessage = "Power Shot Activated!";
+            break;
+            
+        case PowerUpType::SPEED_BOOST:
+            activePowerUps.emplace_back(PowerUpType::SPEED_BOOST, 12.0f);
+            player.setSpeedMultiplier(0.6f);
+            powerUpMessage = "Speed Boost Activated!";
+            break;
+            
+        case PowerUpType::INVINCIBILITY:
+            activePowerUps.emplace_back(PowerUpType::INVINCIBILITY, 10.0f);
+            powerUpMessage = "Invincibility Activated!";
+            break;
+            
+        default:
+            score += 100;
+            powerUpMessage = "+100 Points!";
+            break;
+    }
+}
+
 bool PowerUpManager::hasPowerUpEffect(PowerUpType type) const {
     for (const auto& effect : activePowerUps) {
         if (effect.type == type && effect.active) {

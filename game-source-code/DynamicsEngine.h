4924f50df07f36873c4180a07a7f5386d3601fe1
Erick Chauke <erickchauke0217@gmail.com>
2025-09-28 14:10:39 +0200

    feat: update DynamicsEngine header with new methods

--- game-source-code/DynamicsEngine.h
+++ game-source-code/DynamicsEngine.h
@@ -5,19 +5,28 @@
 #include "BlockGrid.h"
 #include <vector>
 
+// Forward declarations
 class GameObject;
+class DropItem;
 class Collidable;
 
+/**
+ * @brief Manages dynamic objects and interactions
+ */
 class DynamicsEngine {
 public:
     DynamicsEngine();
     
+    void handleFallingObjects(std::vector<DropItem>& items, const BlockGrid& terrain);
     void processAllInteractions(const std::vector<GameObject*>& entities);
     bool checkObjectContact(const Collidable& objA, const Collidable& objB);
     bool checkTerrainCollision(Coordinate position, Coordinate bounds, const BlockGrid& terrain);
     void resolveTerrainCollision(GameObject& object, const BlockGrid& terrain);
     std::vector<Coordinate> getAdjacentPositions(Coordinate center) const;
     bool isPositionSafe(Coordinate position, const BlockGrid& terrain) const;
+
+private:
+    void applyGravity(DropItem& item, const BlockGrid& terrain);
 };
 
-#endif
+#endif // DYNAMICSENGINE_H
